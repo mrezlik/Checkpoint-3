a78a282c98019e9c6412d393b902022e96d7240e
mrezlik <mrezlik@github.com>
2018-02-19 12:16:15 +0100

    Add searching menu in BookController

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -4,6 +4,8 @@ import DAO.BookDAO;
 import DAO.BookDAOSQL;
 import view.View;
 
+import java.sql.SQLException;
+
 public class BookController {
 
     View view = new View();
@@ -54,8 +56,38 @@ public class BookController {
         view.printMessage("In progress");
     }
 
-    public void searchInBooks(){
-        view.printMessage("In progress");
+    private void searchInBooks() throws SQLException{
+        boolean is_searching = true;
+        view.printMessage("What world do you search?");
+        String world = view.getUserInput();
+        while(is_searching) {
+            view.showSearchMenu();
+            String user_choice = view.getUserInput();
+            switch(user_choice){
+                case "1":
+                    view.showBooks(bookDAO.searchByISBN(world));
+                    break;
+                case "2":
+                    view.showBooks(bookDAO.searchByTitle(world));
+                    break;
+                case "3":
+                    view.showBooks(bookDAO.searchByAuthor(world));
+                    break;
+                case "4":
+                    view.showBooks(bookDAO.searchByPublicationYear(world));
+                    break;
+                case "5":
+                    view.showBooks(bookDAO.searchByPublishersName(world));
+                    break;
+                case "6":
+                    is_searching = false;
+                    break;
+                default:
+                    view.printMessage("Something wrong! Try again!");
+                    break;
+
+            }
+        }
     }
 
     public void seeAllBooks(){
