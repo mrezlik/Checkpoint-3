f0776ffca52ee3be7915d646a0d0549ff8ccb3f6
mrezlik <mrezlik@github.com>
2018-02-19 10:45:48 +0100

    Add add new book method in BookDAO

--- src/DAO/BookDAO.java
+++ src/DAO/BookDAO.java
@@ -7,7 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class BookDAO {
 
@@ -18,15 +17,17 @@ public class BookDAO {
         connection = databaseConnection.getConnection();
     }
 
-    public Iterator getAllBooks() throws SQLException{
+    public BookIterator getAllBooks() throws SQLException{
         ArrayList<Book> books = new ArrayList<>();
+
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT ISBN, Authors.name as 'author_name', title, Publishers.name as 'publisher_name'," +
                 " publication_year, price, TypeBooks.type as 'type' FROM Books JOIN Authors ON Books.author = Authors.author_id" +
                 " JOIN Publishers ON Books.publisher = Publishers.publisher_id " +
                 "JOIN TypeBooks ON Books.type = TypeBooks.type_id ORDER BY title;");
         ResultSet queryResult = preparedStatement.executeQuery();
+
         while (queryResult.next()) {
-            int ISBN = queryResult.getInt("ISBN");
+            String ISBN = queryResult.getString("ISBN");
             String author = queryResult.getString("author_name");
             String title = queryResult.getString("title");
             String publisher = queryResult.getString("publisher_name");
@@ -37,7 +38,22 @@ public class BookDAO {
             Book newBook = new Book(ISBN, author, title, publisher, publication_year, price, type);
             books.add(newBook);
         }
+
         return new BookIterator(books);
+    }
+
+    public void addNewBook(Book book) throws SQLException{
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Books(ISBN, author, title, publisher, publication_year, price, type)" +
+                " VALUES (?, ?, ?, ?, ?, ?, ?)");
+        preparedStatement.setString(1, book.getISBN());
+        preparedStatement.setInt(2, getAuthorId(book.getAuthor()));
+        preparedStatement.setString(3, book.getTitle());
+        preparedStatement.setInt(4, getPublisherID(book.getPublisher()));
+        preparedStatement.setInt(5, book.getPublication_year());
+        preparedStatement.setInt(6, book.getPrice());
+        preparedStatement.setInt(7, getTypeID(book.getType()));
+
+        preparedStatement.executeUpdate();
     }
 
 
